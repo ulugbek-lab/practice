503e14d8180087e6b3775eaf67ef4e70f7965780
ulugbek-lab <bekievbek02@gmail.com>
2026-05-07 22:32:52 +0900

    feat: Learning numbes

---
+++ primitive.py
@@ -0,0 +1,13 @@
+print("====== number ======")
+# Everything is an object in Python
+# JAVA: Variable is a name of a storage location!
+# PYTHON: VAriable is a named reference
+
+
+count = 100
+count_type = type(count)
+print(f"the count: {count} and {count_type}")
+
+add1 = count.numerator  # state
+add2 = count.bit_count()  # Method\
+print(add1, add2)
