97a050836dd970eeedf220d9c00e55bf6eec55c0
ulugbek-lab <bekievbek02@gmail.com>
2026-05-07 22:48:59 +0900

    feat: Learn python string

--- primitive.py
+++ primitive.py
@@ -1,13 +1,33 @@
-print("====== number ======")
+# print("====== number ======")
 # Everything is an object in Python
 # JAVA: Variable is a name of a storage location!
 # PYTHON: VAriable is a named reference
 
 
-count = 100
-count_type = type(count)
-print(f"the count: {count} and {count_type}")
+# count = 100
+# count_type = type(count)
+# print(f"the count: {count} and {count_type}")
 
-add1 = count.numerator  # state
-add2 = count.bit_count()  # Method\
-print(add1, add2)
+# add1 = count.numerator  # state
+# add2 = count.bit_count()  # Method\
+# print(add1, add2)
+
+
+
+
+
+print("==== string =====")
+
+
+
+#METHODS: upper() lower() title() find() replace()
+course = "AI Python"
+result = type(course)
+print(f"the type of course: {result}")
+
+result = course.title()
+print(result)
+result = course.upper()
+print(result)
+result = course.replace("Python", "JAVA")
+print(result)
