f68980858a5ac657ed66667a6ea18b526c9b9cb4
ulugbek-lab <bekievbek02@gmail.com>
2026-05-10 19:08:07 +0900

    feat: Learn Py Objects

---
+++ objects.py
@@ -0,0 +1,28 @@
+'''OBJECT
+1. What is object 
+2. Iterable object & Range 
+3. Dictionary 
+4. Error handling system
+'''
+
+import array  # package/module
+import math  # package
+from math import floor, ceil
+print("====what is object====")
+# An object has state and method properties
+# Everything is an object '
+
+
+print(type('hello world'))
+print(type(21))
+print(type(True))
+print(type(array))
+print(type(math))
+
+
+# Paradigm > Functional Programming & OOP
+# OOP 4 concepts > Abstraction | encapsulation | inheritance | Polimorphism
+result1 = math.floor(97.7)  # Call
+print(result1)
+result1 = math.ceil(97.7)  # Call
+print(result1)
