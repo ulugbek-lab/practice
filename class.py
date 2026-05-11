8d809f246e9ed472455315d81c490376d9653b6f
ulugbek-lab <bekievbek02@gmail.com>
2026-05-11 20:18:05 +0900

    feat: Pyhton special/magic methods

--- class.py
+++ class.py
@@ -1,7 +1,7 @@
 '''CLASS
 1. WHat is class
 2. Ordinary vs static properties 
-3. Special methods 
+3. Special (magic)methods 
 
 '''
 print("==============what is class================")
@@ -14,6 +14,7 @@ class Person():
     massage = "class static state property"
 
     # constructor
+    # __init___ this special(maigic) method of python is used fro construction
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -48,3 +49,50 @@ print(person_mas)
 
 # static method
 Person.explain()
+
+
+print("===============Special magic methods ===============")
+# These methods:
+# __init__   __new__  __str__  __call__  __getItem__  __eq__  __len__...
+
+
+class Car():
+    # state
+    description = "this class makes cars"
+
+    # constructor
+    def __new__(cls, *args):
+        print("__init__ is printed")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"{self.name} started the engine")
+
+    def stop_engine(self):
+        print(f"{self.name} stopped  the engine")
+
+    def __str__(self):  # is used to define the informal, human-readable string representation of an object such as Tesla object below
+        # its a return method
+        return f"the car.name {self.name} was produced in {self.year}"
+
+    def __call__(self):  # to call an object as a function such as your_car() method
+        print("object called as function")
+        return True
+
+
+my_car = Car("ferrari", 2026)
+my_car.start_engine()
+my_car.stop_engine()
+
+
+print("------------")
+your_car = Car("tesla", 2121)
+print(your_car)
+
+response = your_car()  # object called as function
+print(response)
