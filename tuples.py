dd424809ab866e2aca519c4736f7e3c58d81c9be
ulugbek-lab <bekievbek02@gmail.com>
2026-05-16 17:46:49 +0900

    feat: PY Tuples

---
+++ tuples.py
@@ -0,0 +1,33 @@
+'''TUPLE
+1. What is tuple 
+2. unpacking arguments 
+3. zip
+'''
+
+print("======what is tuple======")
+# Java/PHP/Node.js => Python list = array
+# Literal\
+
+numbs = [3, 5, 1, 3]
+print(numbs)
+# constructor function
+letters = list("hello world")
+print(letters)
+
+
+fruits = ["apple", "lemon", "cherry", "kiwi"]
+print("before fruits:", fruits)
+
+fruits[2] = "melon"
+print("after:", fruits)
+
+# !!!WE can not change tuple (unmutable)
+animals = ("dog", "cat", "fish", "lion")
+tuple_obj = ("mit", 100, True, None)
+# print(animals[0])
+# animals[0] = "bird"
+# print(animals[0])
+
+print(tuple_obj[2])
+tuple_obj[1] = 22
+print(tuple_obj[2])
