58267c7fe7f43a3a45c154a598abe2496a9a9fa0
ulugbek-lab <bekievbek02@gmail.com>
2026-05-20 18:02:10 +0900

    feat: Pip & external packages

--- debugging.py
+++ debugging.py
@@ -4,6 +4,7 @@
 3. Debugging 
 '''
 
+from PIL import Image
 import turtle
 
 print("======Python Packages & Core, Package ======")
@@ -19,14 +20,31 @@ print("======Python Packages & Core, Package ======")
 # turtle.done()
 
 print("----------")
-my_file = open("material/message.txt", "r")
-try:
-    content = my_file.read()
-    print(content)
-finally:
-    my_file.close()
-# with - Context Manager
-with open("material/message.txt", "r") as your_file:
-    your_content = your_file.read()
-    print(your_content)
-print("Done")
+# my_file = open("material/message.txt", "r")
+# try:
+#     content = my_file.read()
+#     print(content)
+# finally:
+#     my_file.close()
+# # with - Context Manager
+# with open("material/message.txt", "r") as your_file:
+#     your_content = your_file.read()
+#     print(your_content)
+# print("Done")
+
+
+print("======. External Package ======")
+
+# EXTERNAL Packages https://pypi.org/
+'''Package Manager > pip (python)
+python > pip pipenv 
+nodeJS > npm yarn
+PHP > composer
+MacOS > brew 
+'''
+
+
+with Image.open("material/me.png") as img_obj:
+    resized_img = img_obj.resize((400, 200))
+    resized_img.show()
+    resized_img.save("material/sample.png")
